7d0a7ace8a140df57361fbb0a7821a072a7eef54
Kavin Gupta <kavin.2428cs1494@kiet.edu>
2026-01-29 10:54:00 +0530

    Updated JS code

--- file1.js
+++ file1.js
@@ -1,5 +1,19 @@
-var user="Guest";
-function showuser(){
-console.log("user is",user);
-}
-showuser();
+const aa = () => {
+    return { user: "name" };
+};
+console.log(aa());
+const bb = () => [1, 2, 3, 4, 5, 6];
+console.log(bb());
+const user = {
+    name: "aj",
+    age: 23,
+    welcome: function () {
+        console.log(`hello ${this.name}`);
+        console.log(this);
+    }
+};
+
+user.welcome();
+
+user.name = "ajay";
+user.welcome();
