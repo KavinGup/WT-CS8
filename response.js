b1757d261202108c3b3c411cbcb635935afb9ee1
Kavin Gupta <kavin.2428cs1494@kiet.edu>
2026-02-10 11:46:13 +0530

    respose.js

---
+++ response.js
@@ -0,0 +1,74 @@
+// // function add(...numbers){//by default array cretaed rest operater
+// //     let total =0;
+// //     for(let i=0;i<numbers.length;i++){
+// //         // total = total + numbers[i];
+
+// //     }
+// //     return total;
+// // }
+// // console.log(add(1,2,3,4,5));
+
+
+// // function add(...numbers) { // by default array created
+// //     let total = 0;
+    
+// //     for (let num of numbers) {
+// //         total += num;
+// //     }
+    
+// //     return total;
+// // }
+
+// // console.log(add(1, 2, 3, 4, 5)); // 15
+
+
+
+// // let a = [1,2,3,34,45,88];
+// // let [first,sec,last] = a;
+// // console.log(first,sec,last);
+
+
+// // let a = [1,2,3,34,45,88];
+// // let [first,sec, ...last] = a;
+// // console.log(first,sec,...last); 
+
+// let arr1 = [1,2,3];
+// let arr2 = [4,5,6];
+// let arr3 = [...arr1,...arr2];
+// console.log(arr3);
+
+
+// const number=(n) =>{
+// if(n%2==0){
+//     console.log("even");
+// }
+// else{
+//     console.log("odd");
+// }
+// }
+// number(5);
+
+// const string =(string)=>{
+//     let rev="";
+//     for(let i=string.length-1;i>=0;i--){
+//         rev+=string[i];
+//     }
+//     return rev;
+// }
+
+// reverse of string
+// console.log(string("kavin"));
+
+
+
+
+// const string =(string)=>{
+//     let rev="";
+//     for(let i=string.length-1;i>=0;i--){
+//         rev+=string[i];
+//     }
+//     return rev;
+// }
+// console.log(string("kavin"));
+
+
